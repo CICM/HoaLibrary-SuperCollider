fbb50bd1673ce1c676adcd918c7e4903a78737fb
Claudio Cabral <cabral1349@gmail.com>
2015-12-18 16:43:53 +0100

    finished cmake install for OS X

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -59,7 +59,7 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags.
-compilation_database_folder = ''
+compilation_database_folder = '~/GIT/Hoa-Library-SuperCollider/'
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
